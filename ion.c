8db86cad8b532ded76300ed4b185a3950ef13a46
VephoVandenberg <azamatthebest.cs.py@gmail.com>
2022-01-24 16:27:42 +0300

    Day 3 string interning has been added

--- ion.c
+++ ion.c
@@ -1,10 +1,11 @@
-
+#include <stdbool.h>
 #include <stdio.h>
 #include <stddef.h>
 #include <assert.h>
 #include <stdlib.h>
 #include <ctype.h>
 #include <stdint.h>
+#include <stdarg.h>
 
 
 #define MAX(x, y) ((x) >= (y) ? (x) : (y)) 
@@ -33,6 +34,17 @@ void* xmalloc(size_t num_bytes)
 	return ptr;
 }
 
+void fatal(const char* fmt, ...)
+{
+	va_list args;
+	va_start(args, fmt);
+	printf("FATAL: ");
+	vprintf(fmt, args);
+	printf("\n");
+	va_end(args);
+	exit(1);
+}
+
 // Stretchy buffers by Sean Barret
 
 typedef struct bufHdr
@@ -48,7 +60,7 @@ typedef struct bufHdr
 
 #define buf_len(b) ((b) ? buf__hdr(b)->len : 0)
 #define buf_cap(b) ((b) ? buf__hdr(b)->cap : 0)
-#define buf_push(b, x) (buf__fit(b, 1), b[buf_len(b)] = (x), buf__hdr(b)->len++)
+#define buf_push(b, x) (buf__fit(b, 1), (b)[buf__hdr(b)->len++] = (x))
 #define buf_free(b) ((b) ? free(buf__hdr(b)) : 0)
 
 void *buf__grow(const void* buf, size_t new_len, size_t elem_size)
@@ -87,7 +99,51 @@ void buf_test()
 	buf_free(asdf);
 }
 
+typedef struct internStr
+{
+	size_t len;
+	const char* str;
+} internStr;
+
+static internStr* interns;
+
+const char* str_intern_range(const char *start, const char *end)
+{
+	size_t len = end - start;
+	for (size_t i = 0; i < buf_len(interns); i++)
+	{
+		if (interns[i].len = len && strncmp(interns[i].str, start, len) == 0)
+		{
+			return interns[i].str;
+		}
+	}
+
+	char* str = xmalloc(len + 1);
+	memcpy(str, start, len);
+	str[len] = 0;
+	buf_push(interns, ((internStr) { len, str }));
+	return str;
+}
+
 // Lexing: translating char stream to token stream
+const char* str_intern(const char* str)
+{
+	return str_intern_range(str, str + strlen(str));
+}
+
+void str_intern_test()
+{
+	char x[] = "hello";
+	char y[] = "hello";
+	assert(x != y);
+	const char* px = str_intern(x);
+	const char* py = str_intern(y);
+	assert(px == py);
+	char z[] = "hello";
+	const char* pz = str_intern(z);
+	assert(pz == px);
+}
+
 
 typedef enum tokenKind
 {
@@ -181,5 +237,6 @@ int main(int argc, char **argv)
 {
 	buf_test();
 	lex_test();
+	str_intern_test();
 	return 0;
 }
